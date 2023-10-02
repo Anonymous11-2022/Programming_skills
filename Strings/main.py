8d5c06a8dcd38f1f59059601b7eb76174e8cc3f3
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-10-02 23:55:29 +0200

    Did some textual adjustments on Monday 02-10-2023.

--- Strings/main.py
+++ Strings/main.py
@@ -3,7 +3,7 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 
-# Data types en operators
+# Data types and operators
 
 
 # Exercise: Strings a.k.a. "Soccer on a string"😉 (using operators, casting and strings).
